05e5fbdd9947b223f4d52906454000e0a5a26f0c
Anthony <anthony.price@mail.ru>
2022-10-31 23:42:55 +0300

    add eventListener + classes

--- mycode.js
+++ mycode.js
@@ -1,20 +1,66 @@
+let playerSelection;
+
+class PlayerScoreGame {
+    constructor() {
+        this.scoresSum = 0;
+    }
+    upScore() {
+        this.scoresSum += 1;
+    }
+    downScore() {
+        this.scoresSum -= 1;
+    }
+}
+
+const rockScores = new PlayerScoreGame();
+
+const paperScores = new PlayerScoreGame();
+
+const scissorsScores = new PlayerScoreGame();
+
+
 function getComputerChoice () {
     const gameArray = ["Rock", "Paper", "Scissors"];
     return gameArray[Math.floor(Math.random() * gameArray.length)];
 }
 
-function game() {
+const readyRock = document.getElementById('rockButton');
 
-    const playerSelection = "rOck";
+const readyPaper = document.getElementById('paperButton');
+
+const readyScissors = document.getElementById('scissorsButton');
+
+const rockChoice = readyRock.addEventListener('click', callRock);
+
+const paperChoice = readyPaper.addEventListener('click', callPaper);
+
+const scissorsChoice = readyScissors.addEventListener('click', callScissors);
+
+function game() {
 
     return function playRound (func) {
     const compLower = typeof func === "string" ? func.toLowerCase() : "It's not a string";
     const playerSel = typeof playerSelection === "string" ? playerSelection.toLowerCase() : "It's not a string";
     if (compLower === "paper" && playerSel === "rock") {
+        rockScores.downScore();
         return "You Lose! Paper beats Rock";
     } else if (compLower === "scissors" && playerSel === "rock") {
+        rockScores.upScore();
         return "You win! Rock beats Scissors";
-    } else {
+    } else if (compLower === "scissors" && playerSel === "paper") {
+        paperScores.downScore();
+        return "You lose! Scissors beats Paper";
+    } else if (compLower === "rock" && playerSel === "paper") {
+        paperScores.upScore();
+        return "You win! Paper beats Rock";
+    } else if (compLower === "paper" && playerSel === "scissors") {
+        scissorsScores.upScore();
+        return "You win! Scissors beats Paper";
+    } else if (compLower === "rock" && playerSel === "scissors") {
+        scissorsScores.downScore();
+        return "You lose! Rock beats Scissors";
+    }
+    else {
         return "This is draw";
     }
   };
@@ -22,12 +68,20 @@ function game() {
 
 const startGame = game();
 
-for (let i = 0; i < 5; i++) {
-    console.log(startGame(getComputerChoice()));
+function callRock(e) {
+   const playerRock = e.target.innerHTML === "Rock" ? playerSelection = "Rock" : false;
+   return playerRock ? console.log(startGame(getComputerChoice())) : "NothingROCK";
 }
 
+function callPaper(e) {
+    const playerPaper = e.target.innerHTML === "Paper" ? playerSelection = "Paper" : false;
+    return playerPaper ? console.log(startGame(getComputerChoice())) : "NothingPAPER";
+}
 
-
+function callScissors(e) {
+    const playerScissors = e.target.innerHTML === "Scissors" ? playerSelection = "Scissors" : false;
+    return playerScissors ? console.log(startGame(getComputerChoice())) : "NothingSCISSORS";
+}
 
 
 
