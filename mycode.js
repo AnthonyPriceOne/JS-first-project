8e199b617c989b0b596b057140380ddcccf61c7b
Anthony <anthony.price@mail.ru>
2022-11-02 23:55:13 +0300

    add counter attempts and sum of points

--- mycode.js
+++ mycode.js
@@ -7,9 +7,6 @@ class PlayerScoreGame {
     upScore() {
         this.scoresSum += 1;
     }
-    downScore() {
-        this.scoresSum -= 1;
-    }
 }
 
 const rockScores = new PlayerScoreGame();
@@ -38,26 +35,31 @@ const scissorsChoice = readyScissors.addEventListener('click', callScissors);
 
 function game() {
 
+    let sumAll = 0;
+
     return function playRound (func) {
     const compLower = typeof func === "string" ? func.toLowerCase() : "It's not a string";
     const playerSel = typeof playerSelection === "string" ? playerSelection.toLowerCase() : "It's not a string";
     if (compLower === "paper" && playerSel === "rock") {
-        rockScores.downScore();
         return "You Lose! Paper beats Rock";
     } else if (compLower === "scissors" && playerSel === "rock") {
         rockScores.upScore();
+        sumAll = rockScores.scoresSum + paperScores.scoresSum + scissorsScores.scoresSum;
+        console.log(sumAll);
         return "You win! Rock beats Scissors";
     } else if (compLower === "scissors" && playerSel === "paper") {
-        paperScores.downScore();
         return "You lose! Scissors beats Paper";
     } else if (compLower === "rock" && playerSel === "paper") {
         paperScores.upScore();
+        sumAll = rockScores.scoresSum + paperScores.scoresSum + scissorsScores.scoresSum;
+        console.log(sumAll);
         return "You win! Paper beats Rock";
     } else if (compLower === "paper" && playerSel === "scissors") {
         scissorsScores.upScore();
+        sumAll = rockScores.scoresSum + paperScores.scoresSum + scissorsScores.scoresSum;
+        console.log(sumAll);
         return "You win! Scissors beats Paper";
     } else if (compLower === "rock" && playerSel === "scissors") {
-        scissorsScores.downScore();
         return "You lose! Rock beats Scissors";
     }
     else {
@@ -68,20 +70,29 @@ function game() {
 
 const startGame = game();
 
+let leftAttempts = 5;
+
 function callRock(e) {
-   const playerRock = e.target.innerHTML === "Rock" ? playerSelection = "Rock" : false;
-   return playerRock ? console.log(startGame(getComputerChoice())) : "NothingROCK";
+    const counterRemain = leftAttempts > 0 ? leftAttempts -=1 : "There are no more attempts";
+    console.log(counterRemain);
+    const playerRock = e.target.innerHTML === "Rock" ? playerSelection = "Rock" : false;
+    return playerRock ? console.log(startGame(getComputerChoice())) : "NothingROCK";
 }
 
 function callPaper(e) {
+    const counterRemain = leftAttempts > 1 ? leftAttempts -=1 : "There are no more attempts";
+    console.log(counterRemain);
     const playerPaper = e.target.innerHTML === "Paper" ? playerSelection = "Paper" : false;
     return playerPaper ? console.log(startGame(getComputerChoice())) : "NothingPAPER";
 }
 
 function callScissors(e) {
+    const counterRemain = leftAttempts > 1 ? leftAttempts -=1 : "There are no more attempts";
+    console.log(counterRemain);
     const playerScissors = e.target.innerHTML === "Scissors" ? playerSelection = "Scissors" : false;
     return playerScissors ? console.log(startGame(getComputerChoice())) : "NothingSCISSORS";
 }
+
 
 
 
