52b16895eec6104f4293374fcc0bcf2ae63ee968
Anthony <anthony.price@mail.ru>
2022-12-04 17:44:48 +0300

    semi final

--- mycode.js
+++ mycode.js
@@ -1,5 +1,9 @@
 let playerSelection;
 
+let sumPlayerall = 0;
+
+let sumComputerall = 0;
+
 class PlayerScoreGame {
     constructor() {
         this.scoresSum = 0;
@@ -9,12 +13,11 @@ class PlayerScoreGame {
     }
 }
 
-const rockScores = new PlayerScoreGame();
+const rockPlayerscores = new PlayerScoreGame();
 
-const paperScores = new PlayerScoreGame();
+const paperPlayerscores = new PlayerScoreGame();
 
-const scissorsScores = new PlayerScoreGame();
-
+const scissorsPlayerscores = new PlayerScoreGame();
 
 function getComputerChoice () {
     const gameArray = ["Rock", "Paper", "Scissors"];
@@ -27,42 +30,54 @@ const readyPaper = document.getElementById('paperButton');
 
 const readyScissors = document.getElementById('scissorsButton');
 
-const rockChoice = readyRock.addEventListener('click', callRock);
+readyRock.addEventListener('click', callRock);
 
-const paperChoice = readyPaper.addEventListener('click', callPaper);
+readyPaper.addEventListener('click', callPaper);
 
-const scissorsChoice = readyScissors.addEventListener('click', callScissors);
+readyScissors.addEventListener('click', callScissors);
 
 function game() {
-
-    let sumAll = 0;
 
     return function playRound (func) {
     const compLower = typeof func === "string" ? func.toLowerCase() : "It's not a string";
     const playerSel = typeof playerSelection === "string" ? playerSelection.toLowerCase() : "It's not a string";
     if (compLower === "paper" && playerSel === "rock") {
+        console.log(sumPlayerall);
+        sumComputerall += 1;
+        console.log(sumComputerall);
         return "You Lose! Paper beats Rock";
     } else if (compLower === "scissors" && playerSel === "rock") {
-        rockScores.upScore();
-        sumAll = rockScores.scoresSum + paperScores.scoresSum + scissorsScores.scoresSum;
-        console.log(sumAll);
+        rockPlayerscores.upScore();
+        sumPlayerall = rockPlayerscores.scoresSum + paperPlayerscores.scoresSum + scissorsPlayerscores.scoresSum;
+        console.log(sumPlayerall);
+        console.log(sumComputerall);
         return "You win! Rock beats Scissors";
     } else if (compLower === "scissors" && playerSel === "paper") {
+        console.log(sumPlayerall);
+        sumComputerall += 1;
+        console.log(sumComputerall);
         return "You lose! Scissors beats Paper";
     } else if (compLower === "rock" && playerSel === "paper") {
-        paperScores.upScore();
-        sumAll = rockScores.scoresSum + paperScores.scoresSum + scissorsScores.scoresSum;
-        console.log(sumAll);
+        paperPlayerscores.upScore();
+        sumPlayerall = rockPlayerscores.scoresSum + paperPlayerscores.scoresSum + scissorsPlayerscores.scoresSum;
+        console.log(sumPlayerall);
+        console.log(sumComputerall);
         return "You win! Paper beats Rock";
     } else if (compLower === "paper" && playerSel === "scissors") {
-        scissorsScores.upScore();
-        sumAll = rockScores.scoresSum + paperScores.scoresSum + scissorsScores.scoresSum;
-        console.log(sumAll);
+        scissorsPlayerscores.upScore();
+        sumPlayerall = rockPlayerscores.scoresSum + paperPlayerscores.scoresSum + scissorsPlayerscores.scoresSum;
+        console.log(sumPlayerall);
+        console.log(sumComputerall);
         return "You win! Scissors beats Paper";
     } else if (compLower === "rock" && playerSel === "scissors") {
+        console.log(sumPlayerall);
+        sumComputerall += 1;
+        console.log(sumComputerall);
         return "You lose! Rock beats Scissors";
     }
     else {
+        console.log(sumPlayerall);
+        console.log(sumComputerall);
         return "This is draw";
     }
   };
@@ -70,7 +85,7 @@ function game() {
 
 const startGame = game();
 
-let leftAttempts = 5;
+let leftAttempts = 10;
 
 function callRock(e) {
     const counterRemain = leftAttempts > 0 ? leftAttempts -=1 : "There are no more attempts";
